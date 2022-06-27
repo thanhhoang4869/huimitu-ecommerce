dd3d4464094e3ee7a5ca043c4b2c40ba2d092653
LongMyDu <longmydu@gmail.com>
2022-06-27 21:35:32 +0700

    Submit login request to server

--- src/frontend/src/pages/LogInPage/index.jsx
+++ src/frontend/src/pages/LogInPage/index.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+import huimitu from '../../api/huimitu';
+import sha256 from 'crypto-js/sha256';
+import Base64 from 'crypto-js/enc-base64';
 
 const LogInPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmit = async(e) => {
+    e.preventDefault()
+
+    const hashedPassword = Base64.stringify(sha256(password));
+
+    const json = JSON.stringify({ email, password: hashedPassword });
+    console.log(json)
+    const response = await huimitu.post('/login',json)
+
+    alert(response.data.message)
+    console.log(response.data)
+  }
+
   return (
     <div className="d-flex container justify-content-center my-5">
       <form
         className="d-flex flex-column justify-content-center align-items-center form_container col-xl-4 col-md-6 col-xs-12 row"
-        onSubmit={() => {}}
+        onSubmit={onSubmit}
       >
         <h2 className="mb-4 text-key">LOG IN</h2>
 
@@ -15,23 +34,22 @@ const LogInPage = () => {
             name="email"
             type="email"
             placeholder="Email"
-            value={() => {}}
-            onChange={() => {}}
+            value = {email}
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
         <div className="login-input d-flex align-items-center input-group mb-3 p-2">
-          <i class="fa fa-lock"></i>
+          <i className="fa fa-lock"></i>
           <input
             name="password"
             type="password"
             placeholder="Password"
-            value={() => {}}
-            onChange={() => {}}
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
-          <i class="fa fa-eye-slash ml-auto "></i>
         </div>
         <a className="login-forget-link mb-3 align-self-end">Forgotten password?</a>
-        <button className="primary-btn bg-key login-btn" type="submit">
+        <button className="primary-btn bg-key login-btn col-6" type="submit">
           Log in
         </button>
       </form>
