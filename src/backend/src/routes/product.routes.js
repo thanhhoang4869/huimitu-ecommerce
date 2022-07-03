6cd6c06009c813bf006baa66ba59fe82d4bef349
DoVuongPhuc <phuc16102001@gmail.com>
2022-07-04 01:16:15 +0700

    Api newest product (#20)
    * Update database with integrity contraints
    
    * Done newest products and fix the best seller product to match new db

--- src/backend/src/routes/product.routes.js
+++ src/backend/src/routes/product.routes.js
@@ -2,6 +2,7 @@ import product from '#src/controller/product.controller'
 import express from 'express'
 
 const router = express.Router();
-router.get('/best-seller', product.getBestSeller)
+router.get('/bestSeller', product.getBestSeller)
+router.get('/newestArrival', product.getNewestArrival)
 
 export default router;
