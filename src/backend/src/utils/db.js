51aa3cf8a209c5187f61a80403d586552b6fa11f
phuc16102001 <phuc16102001@gmail.com>
2022-07-07 17:51:35 +0700

    allow non ssl connection

--- src/backend/src/utils/db.js
+++ src/backend/src/utils/db.js
@@ -1,12 +1,16 @@
 import config from '#src/config/config'
 import knex from 'knex'
 
+// For heroku deployment
 const environment = process.env.NODE_ENV || "development"
-const connection = (environment === "developement") ? (config.DATABASE) : (process.env.DATABASE_URL);
+const connection = (environment === "development") ? (config.DATABASE) : (process.env.DATABASE_URL);
 
 const pg = knex({
     client: 'pg',
     connection: connection,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default pg 
