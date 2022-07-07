421237dffa16c166293a6cfd91ce31d55b29b3b9
phuc16102001 <phuc16102001@gmail.com>
2022-07-07 14:29:30 +0700

    Done add, get product image

--- src/backend/src/config/config.js
+++ src/backend/src/config/config.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 const config = {
   DATABASE: {
     host: "localhost",
@@ -15,6 +14,15 @@ const config = {
     ADMIN: 'admin'
   },
 
+  COULDINARY_CONFIG: {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  },
+
+  CLOUDINARY_PRODUCT_PATH: "huimitu/product/",
+  CLOUDINARY_AVATAR_PATH: "huimitu/avatar/",
+
   PORT: 8080,
   JWT_EXP_TIME: 60 * 60 * 24,
   JWT_SECRET: process.env.JWT_SECRET,
@@ -26,6 +34,7 @@ const config = {
   GMAIL_USERNAME: process.env.GMAIL_USERNAME,
 
   BEST_SELLER_LIMIT: 4,
+  IMAGE_NUMBER_LIMIT: 4
 };
 
 export default config;
--- src/backend/src/routes/product.routes.js
+++ src/backend/src/routes/product.routes.js
@@ -2,13 +2,27 @@ import product from '#src/controller/product.controller'
 import verifyLogin from '#src/middlewares/verifyLogin.mdw'
 import verifyAdmin from '#src/middlewares/verifyAdmin.mdw'
 import express from 'express'
+import { createUploader } from '#src/utils/cloudinary'
+import config from '#src/config/config'
+
+const productImageUploader = createUploader(
+    config.CLOUDINARY_PRODUCT_PATH,
+    ["png", "jpg"]
+)
 
 const router = express.Router();
 router.get('/bestSeller', product.getBestSeller)
 router.get('/newestArrival', product.getNewestArrival)
 router.post('/getByCategory', product.getProductByCategory)
 
-router.post('/', verifyLogin, verifyAdmin, product.createProduct)
+router.post(
+    '/', 
+    verifyLogin, 
+    verifyAdmin, 
+    productImageUploader.array('productImg', config.IMAGE_NUMBER_LIMIT), 
+    product.createProduct
+)
+
 router.route('/:productId')
     .get(product.getSingleProduct)
     .patch(verifyLogin, verifyAdmin, product.updateProduct)
