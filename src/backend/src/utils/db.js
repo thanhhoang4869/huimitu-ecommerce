e8ecca6760c30d5575d96dce1e3c895e191a2d5b
phuc16102001 <phuc16102001@gmail.com>
2022-07-07 17:41:08 +0700

    Add connection for heroku pgsql

--- src/backend/src/utils/db.js
+++ src/backend/src/utils/db.js
@@ -1,9 +1,12 @@
 import config from '#src/config/config'
 import knex from 'knex'
 
+const environment = process.env.NODE_ENV || "development"
+const connection = (environment === "developement") ? (config.DATABASE) : (process.env.DATABASE_URL);
+
 const pg = knex({
     client: 'pg',
-    connection: config.DATABASE,
+    connection: connection,
 });
 
 export default pg 
