012cc528c8f7d570a699f5aa928e2095e2131e10
phuc16102001 <phuc16102001@gmail.com>
2022-07-08 20:35:41 +0700

    update order id to varchar, state

--- src/backend/src/models/order.model.js
+++ src/backend/src/models/order.model.js
@@ -1,11 +1,42 @@
 import db from '#src/utils/db'
 
 export default {
-    async createOrderByVariant(orderId, variantId, quantity) {
-       
+    async createOrder(email, orderId, entity) {
+        const {
+            paymentId,
+            shippingAddressId,
+            shippingProviderId,
+            voucherId
+        } = entity
+
+        const order = await db('order').insert({
+            id: orderId,
+            email: email,
+            payment_id: paymentId,
+            shipping_address_id: shippingAddressId,
+            shippingProviderId: shippingProviderId,
+            voucherId: voucherId
+        }).returning('id')
+        
+        try {
+            return order[0].id;
+        } catch (err) {
+            return null;
+        }
     },
 
-    async createOrderByCart(orderId, cartId){
-        
+    async insertListVariantToOrder(orderId, listVariant) {
+        const listEntity = listVariant.map(item => ({
+            order_id: orderId,
+            variant_id: item.variantId,
+            variant_price: item.variantPrice,
+            quantity: item.quantity,
+        }))
+        const result = await db('order_variant').insert(listEntity)
+        return result;
+    },
+
+    async createOrderByCart(orderId, cartId) {
+
     }
 }
