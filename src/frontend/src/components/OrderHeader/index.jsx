0497bf287e7efb6b289b763d5df73e5078bc83d0
LongMyDu <longmydu@gmail.com>
2022-07-13 11:44:34 +0700

    Format date

--- src/frontend/src/components/OrderHeader/index.jsx
+++ src/frontend/src/components/OrderHeader/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './style.css'
+import formatter from "utils/formatter";
+
 
 const OrderHeader = ({order}) => {
   return (
     <div className='order-header'>
-        {/* <p className='order-id'>
-            {order.id}
-        </p> */}
         <p className='order-date-created'>
-            {order.created_time}
+            {formatter.formatDate(order.created_time)}
         </p>
         <p className='order-status'>
             Đang vận chuyển
