c03d3ff627fece13fae453f56cada07dd4959a25
phuc16102001 <phuc16102001@gmail.com>
2022-07-13 09:06:44 +0700

    Add get voucher by code with email

--- src/backend/src/models/voucher.model.js
+++ src/backend/src/models/voucher.model.js
@@ -3,7 +3,31 @@ import db from '#src/utils/db'
 export default {
     async getVoucherByCode(voucherCode) {
         const result = await db('voucher').where({
-            voucher_code: voucherCode
+            'voucher_code': voucherCode
+        }).select(
+            "voucher_code",
+            "percentage_discount",
+            "minimum_price",
+            "maximum_discount_price",
+            "start_date",
+            "end_date"
+        )
+        return result[0] || null;
+    },
+
+
+    async getVoucherByCodeEmail(voucherCode, email) {
+        const result = await db('voucher as v1').whereNotIn(
+            'v1.voucher_code',
+            db('order as o').join(
+                'voucher as v2',
+                'o.voucher_code',
+                'v2.voucher_code'
+            ).where({
+                "o.email": email
+            }).select("v2.voucher_code")
+        ).where({
+            'voucher_code': voucherCode
         }).select(
             "voucher_code",
             "percentage_discount",
@@ -46,12 +70,12 @@ export default {
             endDate
         } = entity;
         const result = await db('voucher').insert({
-            voucher_code: voucherCode,
-            percentage_discount: percentageDiscount,
-            minimum_price: minimumPrice,
-            maximum_discount_price: maximumDiscountPrice,
-            start_date: startDate,
-            end_date: endDate
+            'voucher_code': voucherCode,
+            'percentage_discount': percentageDiscount,
+            'minimum_price': minimumPrice,
+            "maximum_discount_price": maximumDiscountPrice,
+            "start_date": startDate,
+            'end_date': endDate
         })
         return result;
     }
