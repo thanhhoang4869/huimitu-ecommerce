2d8e62259ff7b259c694de51a18acad2aa1bcea3
Thanh Hoang <thanhhoang4869@gmail.com>
2022-07-22 00:35:41 +0700

    add form into variant add modal

--- src/frontend/src/pages/AdminPage/AddVariantModal/index.jsx
+++ src/frontend/src/pages/AdminPage/AddVariantModal/index.jsx
@@ -1,15 +1,96 @@
 import { Modal } from "antd";
+import { Button, Form, Input } from "antd";
 
 const AddVariantModal = (props) => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
     <Modal
       title="Thêm biến thể"
+      destroyOnClose={true}
       visible={props.visible}
       onOk={props.handleOk}
       confirmLoading={props.confirmLoading}
       onCancel={props.handleCancel}
+      footer={null}
     >
-      <p>QQ gì dãy</p>
+      <Form
+        name="basic"
+        layout="vertical"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Tên biến thể"
+          name="addVariantName"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng nhập tên!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Giá"
+          name="price"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng nhập giá!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item label="Giá giảm" name="discountPrice">
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Số lượng"
+          name="stock"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng nhập số lượng!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item>
+          <div
+            style={{
+              textAlign: "end",
+            }}
+          >
+            <Button
+              htmlType="button"
+              className="mr-3"
+              onClick={props.handleCancel}
+            >
+              Hủy
+            </Button>
+            <Button type="primary" htmlType="submit">
+              Thêm
+            </Button>
+          </div>
+        </Form.Item>
+      </Form>
     </Modal>
   );
 };
