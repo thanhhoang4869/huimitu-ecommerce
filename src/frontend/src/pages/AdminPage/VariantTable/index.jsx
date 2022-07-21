104dbac36dc7e5420eef0826dc6a740e583c5a1c
Thanh Hoang <thanhhoang4869@gmail.com>
2022-07-21 23:11:14 +0700

    fill data to ui edit product

--- src/frontend/src/pages/AdminPage/VariantTable/index.jsx
+++ src/frontend/src/pages/AdminPage/VariantTable/index.jsx
@@ -2,58 +2,22 @@ import { Table } from "antd";
 import React from "react";
 const columns = [
   {
-    title: "Name",
+    title: "Tên",
     dataIndex: "name",
-    filters: [
-      {
-        text: "Joe",
-        value: "Joe",
-      },
-      {
-        text: "Jim",
-        value: "Jim",
-      },
-      {
-        text: "Submenu",
-        value: "Submenu",
-        children: [
-          {
-            text: "Green",
-            value: "Green",
-          },
-          {
-            text: "Black",
-            value: "Black",
-          },
-        ],
-      },
-    ],
-    // specify the condition of filtering result
-    // here is that finding the name started with `value`
-    onFilter: (value, record) => record.name.indexOf(value) === 0,
-    sorter: (a, b) => a.name.length - b.name.length,
-    sortDirections: ["descend"],
   },
   {
-    title: "Age",
-    dataIndex: "age",
-    defaultSortOrder: "descend",
-    sorter: (a, b) => a.age - b.age,
+    title: "Giá",
+    dataIndex: "price",
+    sorter: (a, b) => a.price - b.price,
   },
   {
-    title: "Address",
-    dataIndex: "address",
-    filters: [
-      {
-        text: "London",
-        value: "London",
-      },
-      {
-        text: "New York",
-        value: "New York",
-      },
-    ],
-    onFilter: (value, record) => record.address.indexOf(value) === 0,
+    title: "Giá giảm",
+    dataIndex: "discountPrice",
+  },
+  {
+    title: "Số lượng",
+    dataIndex: "stock",
+    sorter: (a, b) => a.price - b.price,
   },
 ];
 const data = [
